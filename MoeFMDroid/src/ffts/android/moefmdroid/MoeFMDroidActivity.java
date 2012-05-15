1097b2b40655515406fe8809ce069136299bb1fd
ffts <ffts133@gmail.com>
2012-05-15 09:58:42 +0800

    Init

---
+++ MoeFMDroid/src/ffts/android/moefmdroid/MoeFMDroidActivity.java
@@ -0,0 +1,13 @@
+package ffts.android.moefmdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MoeFMDroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
